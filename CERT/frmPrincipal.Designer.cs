c9c4fa09f305cc93a8404aa4f2a579090ebe4956
Renato Santos <renatosantoslw@gmail.com>
2024-08-16 17:14:31 -0500

    Primeiro Commit

---
+++ CERT/frmPrincipal.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace CERT
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPrincipal));
+            this.btnAcao = new System.Windows.Forms.Button();
+            this.btnAcao2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictBoxLogoPainel = new System.Windows.Forms.PictureBox();
+            this.lblPainel = new System.Windows.Forms.Label();
+            this.pictBoxPainel = new System.Windows.Forms.PictureBox();
+            this.txtCert = new System.Windows.Forms.TextBox();
+            this.lblIncluir = new System.Windows.Forms.LinkLabel();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictBoxLogoPainel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictBoxPainel)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAcao
+            // 
+            this.btnAcao.Location = new System.Drawing.Point(533, 391);
+            this.btnAcao.Name = "btnAcao";
+            this.btnAcao.Size = new System.Drawing.Size(91, 23);
+            this.btnAcao.TabIndex = 0;
+            this.btnAcao.Text = "Abrir";
+            this.btnAcao.UseVisualStyleBackColor = true;
+            this.btnAcao.Click += new System.EventHandler(this.btnAcao_Click);
+            // 
+            // btnAcao2
+            // 
+            this.btnAcao2.Location = new System.Drawing.Point(436, 391);
+            this.btnAcao2.Name = "btnAcao2";
+            this.btnAcao2.Size = new System.Drawing.Size(91, 23);
+            this.btnAcao2.TabIndex = 2;
+            this.btnAcao2.Text = "Salvar";
+            this.btnAcao2.UseVisualStyleBackColor = true;
+            this.btnAcao2.Click += new System.EventHandler(this.btnAcao2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Certificado String:";
+            // 
+            // pictBoxLogoPainel
+            // 
+            this.pictBoxLogoPainel.Image = ((System.Drawing.Image)(resources.GetObject("pictBoxLogoPainel.Image")));
+            this.pictBoxLogoPainel.Location = new System.Drawing.Point(12, 10);
+            this.pictBoxLogoPainel.Margin = new System.Windows.Forms.Padding(2);
+            this.pictBoxLogoPainel.Name = "pictBoxLogoPainel";
+            this.pictBoxLogoPainel.Size = new System.Drawing.Size(48, 48);
+            this.pictBoxLogoPainel.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictBoxLogoPainel.TabIndex = 61;
+            this.pictBoxLogoPainel.TabStop = false;
+            // 
+            // lblPainel
+            // 
+            this.lblPainel.AutoSize = true;
+            this.lblPainel.BackColor = System.Drawing.Color.Transparent;
+            this.lblPainel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPainel.ForeColor = System.Drawing.Color.White;
+            this.lblPainel.Location = new System.Drawing.Point(71, 24);
+            this.lblPainel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblPainel.Name = "lblPainel";
+            this.lblPainel.Size = new System.Drawing.Size(238, 24);
+            this.lblPainel.TabIndex = 60;
+            this.lblPainel.Text = "Instalador Certificado Digital";
+            // 
+            // pictBoxPainel
+            // 
+            this.pictBoxPainel.BackColor = System.Drawing.Color.SteelBlue;
+            this.pictBoxPainel.Location = new System.Drawing.Point(-1, -1);
+            this.pictBoxPainel.Margin = new System.Windows.Forms.Padding(2);
+            this.pictBoxPainel.Name = "pictBoxPainel";
+            this.pictBoxPainel.Size = new System.Drawing.Size(726, 70);
+            this.pictBoxPainel.TabIndex = 59;
+            this.pictBoxPainel.TabStop = false;
+            // 
+            // txtCert
+            // 
+            this.txtCert.BackColor = System.Drawing.SystemColors.Control;
+            this.txtCert.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCert.Location = new System.Drawing.Point(23, 102);
+            this.txtCert.Margin = new System.Windows.Forms.Padding(2);
+            this.txtCert.Multiline = true;
+            this.txtCert.Name = "txtCert";
+            this.txtCert.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtCert.Size = new System.Drawing.Size(607, 213);
+            this.txtCert.TabIndex = 63;
+            // 
+            // lblIncluir
+            // 
+            this.lblIncluir.AutoSize = true;
+            this.lblIncluir.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIncluir.LinkBehavior = System.Windows.Forms.LinkBehavior.HoverUnderline;
+            this.lblIncluir.LinkColor = System.Drawing.Color.SteelBlue;
+            this.lblIncluir.Location = new System.Drawing.Point(580, 338);
+            this.lblIncluir.Name = "lblIncluir";
+            this.lblIncluir.Size = new System.Drawing.Size(47, 15);
+            this.lblIncluir.TabIndex = 64;
+            this.lblIncluir.TabStop = true;
+            this.lblIncluir.Text = "Instalar";
+            this.lblIncluir.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lblIncluir_LinkClicked);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.LinkBehavior = System.Windows.Forms.LinkBehavior.HoverUnderline;
+            this.linkLabel1.LinkColor = System.Drawing.Color.SteelBlue;
+            this.linkLabel1.Location = new System.Drawing.Point(480, 338);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(85, 15);
+            this.linkLabel1.TabIndex = 65;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Converter PFX";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(655, 375);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.lblIncluir);
+            this.Controls.Add(this.txtCert);
+            this.Controls.Add(this.pictBoxLogoPainel);
+            this.Controls.Add(this.lblPainel);
+            this.Controls.Add(this.pictBoxPainel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnAcao2);
+            this.Controls.Add(this.btnAcao);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frmPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CERT";
+            this.Load += new System.EventHandler(this.frmPrincipal_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictBoxLogoPainel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictBoxPainel)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAcao;
+        private System.Windows.Forms.Button btnAcao2;
+        private System.Windows.Forms.Label label1;
+        internal System.Windows.Forms.PictureBox pictBoxLogoPainel;
+        internal System.Windows.Forms.Label lblPainel;
+        internal System.Windows.Forms.PictureBox pictBoxPainel;
+        internal System.Windows.Forms.TextBox txtCert;
+        internal System.Windows.Forms.LinkLabel lblIncluir;
+        internal System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ CERT/frmPrincipal.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.IO;
+using System.Drawing.Drawing2D;
+
+namespace CERT
+{
+    public partial class frmPrincipal : Form
+    {
+
+        frmInputBox frm2 = new frmInputBox();
+
+
+        public frmPrincipal()
+        {
+            InitializeComponent();
+    
+        }
+
+        private void btnAcao_Click(object sender, EventArgs e)
+        {
+
+
+            try
+            {
+
+
+
+                OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+                openFileDialog1.Filter = "Arquivo do tipo pfx (*.pfx)|*.pfx";
+
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+
+                    string filename = openFileDialog1.FileName;
+                    var cert = new X509Certificate2(filename, frm2.strSenhaCertificado, X509KeyStorageFlags.Exportable);
+                    var stringOfCertWithPrivateKey = Convert.ToBase64String(cert.Export(X509ContentType.Pkcs12));
+
+                    X509Store store = new X509Store(StoreName.My);
+                    store.Open(OpenFlags.ReadWrite);
+
+                    store.Remove(cert);
+                    MessageBox.Show("Certificado Removido!");
+
+                    store.Add(cert);
+                    txtCert.Text = stringOfCertWithPrivateKey;
+
+                    MessageBox.Show("Certificado Instalado!");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            // string filename = @"C:\D\SANTOS DO VALE COMERCIO LTDA.pfx";
+            // var cert = new X509Certificate2(filename,"12345678", X509KeyStorageFlags.Exportable);         
+            // var stringOfCertWithPrivateKey = Convert.ToBase64String(cert.Export(X509ContentType.Pkcs12));
+            //var stringOfCertWithoutPrivateKey = Convert.ToBase64String(cert.Export(X509ContentType.Cert));
+            // txtVarcharCert.Text= stringOfCertWithPrivateKey;
+
+        }
+
+        private void btnAcao2_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+
+                var certBytes = Convert.FromBase64String(txtCert.Text);
+                // var cert = new X509Certificate2(certBytes);
+                var cert = new X509Certificate2(certBytes, (string)null, X509KeyStorageFlags.Exportable);
+
+                //File.WriteAllBytes(@"C:\D\EXPORTADO.pfx", cert.Export(X509ContentType.Pkcs12, "12345678"));
+
+                SaveFileDialog file = new SaveFileDialog();
+
+
+                file.Filter = "Arquivo do tipo pfx (*.pfx)|*.pfx";
+                file.ShowDialog();
+
+                if (file.FileName != "")
+                {
+                    File.WriteAllBytes(file.FileName, cert.Export(X509ContentType.Pkcs12, frm2.strSenhaCertificado));
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+
+
+
+        }
+
+        private void frmPrincipal_Load(object sender, EventArgs e)
+        {
+            Bitmap img = new Bitmap(pictBoxPainel.Width, pictBoxPainel.Height);
+            LinearGradientBrush brush = new System.Drawing.Drawing2D.LinearGradientBrush(new PointF(0, 0), new PointF(img.Width, img.Height), Color.SteelBlue, SystemColors.Control);
+            Graphics gr  = Graphics.FromImage(img);
+            gr.FillRectangle(brush, new RectangleF(0, 0, img.Width, img.Height));
+            pictBoxPainel.BackgroundImage = img;
+
+            lblPainel.Parent = pictBoxPainel;
+            lblPainel.BackColor = Color.Transparent;
+            lblPainel.BringToFront();
+
+            pictBoxLogoPainel.Parent = pictBoxPainel;
+            pictBoxLogoPainel.BackColor = Color.Transparent;
+            pictBoxLogoPainel.BringToFront();
+
+        }
+
+        private void lblIncluir_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            frm2.ShowDialog();
+
+            try
+            {
+
+                OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+                openFileDialog1.Filter = "Arquivo do tipo pfx (*.pfx)|*.pfx";
+
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+
+                    string filename = openFileDialog1.FileName;
+                    var cert = new X509Certificate2(filename, frm2.strSenhaCertificado, X509KeyStorageFlags.Exportable);
+                    var stringOfCertWithPrivateKey = Convert.ToBase64String(cert.Export(X509ContentType.Pkcs12));
+
+                    X509Store store = new X509Store(StoreName.My);
+                    store.Open(OpenFlags.ReadWrite);
+
+                    store.Remove(cert);
+                    MessageBox.Show("Certificado Removido!");
+
+                    store.Add(cert);
+                    txtCert.Text = stringOfCertWithPrivateKey;
+
+                    MessageBox.Show("Certificado Instalado!");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+
+
+
+            // string filename = @"C:\D\SANTOS DO VALE COMERCIO LTDA.pfx";
+            // var cert = new X509Certificate2(filename,"12345678", X509KeyStorageFlags.Exportable);         
+            // var stringOfCertWithPrivateKey = Convert.ToBase64String(cert.Export(X509ContentType.Pkcs12));
+            //var stringOfCertWithoutPrivateKey = Convert.ToBase64String(cert.Export(X509ContentType.Cert));
+            // txtVarcharCert.Text= stringOfCertWithPrivateKey;
+
+
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+
+            frm2.ShowDialog();
+
+      
+
+            try
+            {
+
+                var certBytes = Convert.FromBase64String(txtCert.Text);
+                // var cert = new X509Certificate2(certBytes);
+                var cert = new X509Certificate2(certBytes, (string)null, X509KeyStorageFlags.Exportable);
+
+                //File.WriteAllBytes(@"C:\D\EXPORTADO.pfx", cert.Export(X509ContentType.Pkcs12, "12345678"));
+
+                SaveFileDialog file = new SaveFileDialog();
+
+
+                file.Filter = "Arquivo do tipo pfx (*.pfx)|*.pfx";
+                file.ShowDialog();
+
+                if (file.FileName != "")
+                {
+                    File.WriteAllBytes(file.FileName, cert.Export(X509ContentType.Pkcs12, frm2.strSenhaCertificado));
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+
+
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+          
+           
+       
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_DoubleClick(object sender, EventArgs e)
+        {
+      
+
+        }
+    }
+
+
+
+}
